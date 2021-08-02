bc4ebc8bb18e504ca56ec6c7a0d8fda5287b9d5c
Sameer <mdsam55555@gmail.com>
2021-08-02 18:19:52 +0530

    Again testing2

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use("/dsasheet", frazRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV == "production") {
   app.use(express.static("client/build"));
   const path = require("path");
   app.get("*", (req, res) => {
