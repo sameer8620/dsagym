62c5b70950986e29e78a88ecf01b2f2083cdc221
Sameer <mdsam55555@gmail.com>
2021-08-02 20:24:54 +0530

    oooooookaaaay(:

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,5 +1,5 @@
-require("dotenv").config();
 import axios from "axios";
+require("dotenv").config();
 
 export default axios.create({
   baseURL: `https://dsagym.herokuapp.com/${process.env.API_KEY}`,
