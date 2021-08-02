d8d199486e852437457c90fff35222019cebe85c
Sameer <mdsam55555@gmail.com>
2021-08-02 20:20:43 +0530

    Please work

--- client/src/axios.js
+++ client/src/axios.js
@@ -2,5 +2,5 @@ require("dotenv").config();
 import axios from "axios";
 
 export default axios.create({
-  baseURL: `https://dsagym.herokuapp.com/${process.env.API_KEY}/sdesheet`,
+  baseURL: `https://dsagym.herokuapp.com/${process.env.API_KEY}`,
 });
